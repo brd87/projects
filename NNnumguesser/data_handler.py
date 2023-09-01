8e08c0c736b85566828c0f039ef95a9ed771b974
brd87 <115787728+brd87@users.noreply.github.com>
2023-09-01 15:50:22 +0200

    Add files via upload

--- NNnumguesser/data_handler.py
+++ NNnumguesser/data_handler.py
@@ -1,6 +1,7 @@
 import csv
 
 from network import Network
+from number import Number
 
 class DataHandler:
     def __init__(self):
@@ -16,20 +17,34 @@ class DataHandler:
 
 
     def train_read(self, filename):
-        batch_examples = []
-        with open(f"{filename}.scv", "r") as file:
-            csvreader = csv.reader(file)
-            next(csvreader)
-            for item in csvreader[:self.mini_batch]:
-                batch_examples.append(item)
-        return batch_examples
+        for epoch in range(self.epochs):
+            with open(f"{filename}.csv", "r") as file:
+                batch_examples = []
+                csvr = csv.reader(file, delimiter=',')
+                next(csvr)
+                current_row = 0
+                for current in range(self.data_range):
+                    for row in csvr:
+                        item = [int(val) for val in row]
+                        batch_examples.append(Number(item[0], item[1:]))
+                        current_row += 1
+                        if current_row >= self.mini_batch:
+                            break
+                    self.network.forward_propagation() # preforms forwardpropagation
+                    self.network.back_propagation() # preforms backpropagation
+                    self.network.update_WAB() # preforms update
+                    batch_examples.clear()
 
 
-    def single_read(self, filename):
+    def single_read(self, filename, target):
         item = None
         with open(f"{filename}.scv", "r") as file:
-            pass
-        return item
+            csvr = csv.reader(file, delimiter=',')
+            next(csvr)
+            for skip in range(target):
+                next(csvr)
+            item = [int(val) for val in next(csvr)]
+        return Number(None, item)
 
 
     def load_WAB(self, filename):
--- NNnumguesser/network.py
+++ NNnumguesser/network.py
@@ -1,3 +1,12 @@
-
 class Network:
-    pass
+    def __init__(self):
+        pass
+
+    def forward_propagation(self):
+        pass
+    
+    def back_propagation(self):
+        pass
+    
+    def update_WAB(self):
+        pass
