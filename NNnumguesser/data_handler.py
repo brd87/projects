8dc63520355e41cd31e2812ef82e94c4e9dbc1d3
brd87 <115787728+brd87@users.noreply.github.com>
2023-09-02 14:26:26 +0200

    Add files via upload

--- NNnumguesser/data_handler.py
+++ NNnumguesser/data_handler.py
@@ -2,17 +2,20 @@ import csv
 
 from network import Network
 from number import Number
+from layer import Layer
 
 class DataHandler:
-    def __init__(self):
-        self.network = Network()
+    def __init__(self, is_WAB, filename, hidden_num):
+        WAB = None
+        if is_WAB:
+            WAB = self.load_WAB(filename)
+        self.network = Network(WAB, hidden_num)
 
 
-    def train_setup(self, mini_batch, epochs, learning_rate, is_WAB, data_range):
+    def train_setup(self, mini_batch, epochs, learning_rate, data_range):
         self.mini_batch = mini_batch
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.is_WAB = is_WAB
         self.data_range = data_range
 
 
@@ -30,6 +33,7 @@ class DataHandler:
                         current_row += 1
                         if current_row >= self.mini_batch:
                             break
+                    self.network.load_data(batch_examples)
                     self.network.forward_propagation() # preforms forwardpropagation
                     self.network.back_propagation() # preforms backpropagation
                     self.network.update_WAB() # preforms update
@@ -49,8 +53,22 @@ class DataHandler:
 
     def load_WAB(self, filename):
         WAB = []
-        with open(f"{filename}.scv", "r") as file:
-            pass
+        with open(f"{filename}.txt", "r") as file:
+            bias = []
+            weights = []
+            for line in file:
+                if line == "NEW":
+                    WAB.append(Layer(bias, weights))
+                    bias = []
+                    weights = []
+                    continue
+
+                line = line.split(";")
+                item = [float(val) for val in line]
+                
+                if bias == []:
+                    bias.append(item)
+                weights.append(item)
         return WAB
 
 
--- NNnumguesser/network.py
+++ NNnumguesser/network.py
@@ -1,12 +1,32 @@
+import numpy as np
+
+from number import Number
+from layer import Layer
+
 class Network:
-    def __init__(self):
+    def __init__(self, WAB, hidden_num):
+        self.layers = []
+        if WAB is None:
+            self.generate_WAB(hidden_num)
+        for single_layer in WAB:
+                self.layers.append(single_layer)
+
+
+    def load_data(self, batch_examples):
+        self.batch_examples = batch_examples
+
+
+    def generate_WAB(self, hidden_num):
         pass
+
 
     def forward_propagation(self):
         pass
     
+
     def back_propagation(self):
         pass
     
+
     def update_WAB(self):
         pass
