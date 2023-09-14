979f51014e5f243ec128161160b6ad572ecb7a2b
brd87 <115787728+brd87@users.noreply.github.com>
2023-09-14 16:35:44 +0200

    Delete PGplatformer/goodplatform.py

--- PGplatformer/goodplatform.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from pygame.locals import *
-
-class GoodPlatform:
-    def __init__(self, WIDTH, HEIGHT, color):
-        self.surf = pygame.Surface((WIDTH, 20))
-        self.surf.fill(color)
-        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT-10))
